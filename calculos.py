ac56efacfe41f8ef0d05bfcb8c247846c53dbd1d
Costa <gustavodscostadev@gmail.com>
2026-09-21 19:17:49 -0300

    Adiciona testes para cálculo de tempo de vídeo

--- calculos.py
+++ calculos.py
@@ -6,8 +6,3 @@ def calcular_tempo_video(duracao_minutos, velocidade):
         raise ValueError("A duração do video deve ser maior que zero.")
     
     return duracao_minutos / velocidade
-
-
-
-
-print(calcular_tempo_video(30, 1.5))
---
+++ test_calculos.py
@@ -0,0 +1,7 @@
+from calculos import calcular_tempo_video
+
+assert calcular_tempo_video(30,1.5) == 20
+
+assert calcular_tempo_video(30,2.0) == 15
+
+print("Teste passou")
